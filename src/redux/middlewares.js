c4d9df1934172a8ef6c2667e4adb73a5f4cae84f
Hotinec Artiom <hotinec2@gmail.com>
2020-12-15 23:39:26 +0300

    fix: middleWare

--- src/redux/middlewares.js
+++ src/redux/middlewares.js
@@ -2,6 +2,6 @@ import { set } from './covidInfoSlice';
 import { apiRequest } from '../services/api';
 
 export const fetchCovidInfo = () => async dispatch => {
-const apiResponse = await apiRequest('https://api.covid19api.com/summary');
-dispatch(set(apiResponse.Countries));
+  const apiResponse = await apiRequest('https://api.covid19api.com/summary');
+  dispatch(set(apiResponse.Countries));
 }
