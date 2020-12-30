c187d541ae87487e034e9faa07f0d820660f1422
Hotinec Artiom <hotinec2@gmail.com>
2020-12-30 14:18:57 +0300

    Fix: moved toast to middleware

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
 import Box from "@material-ui/core/Box";
-import { toast } from "react-toastify";
 import { fetchCovidInfo } from "./redux/middlewares";
 import Header from "./Components/header";
 import Dashboard from "./pages/dashboard";
-import { selectInfoError } from "./redux/covidInfoSlice";
+// import { selectInfoError } from "./redux/covidInfoSlice";
 import GlobalCases from "./Components/global-cases";
 import CountiesList from "./Components/countries-list";
 import Map from "./Components/map";
@@ -15,30 +14,14 @@ import CaseChart from "./Components/case-chart";
 import { selectCurrentBoard } from "./redux/currentBoardSlice";
 import "react-toastify/dist/ReactToastify.css";
 
-toast.configure();
-
 function App() {
   const dispatch = useDispatch();
   const currentBoard = useSelector(selectCurrentBoard);
-  const apiError = useSelector(selectInfoError);
+  // const apiError = useSelector(selectInfoError);
 
   useEffect(() => {
     dispatch(fetchCovidInfo());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (apiError) {
-      toast.error("API error. Please reload the page or try later.", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  }, [apiError]);
 
   const renderSwitch = (param) => {
     switch (param) {
--- src/redux/middlewares.js
+++ src/redux/middlewares.js
@@ -1,46 +1,75 @@
+import { toast } from "react-toastify";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { apiRequest } from "../services/api";
+
+const TOAST_PARAMS = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
+toast.configure();
 
 export const fetchChartData = createAsyncThunk(
   "chartInfo/fetch",
   async (country) => {
-    let result;
-    if (!country) {
-      result = await apiRequest("https://covid19-api.org/api/timeline");
-    } else {
-      result = await apiRequest(
-        `https://covid19-api.org/api/timeline/${country}`
+    try {
+      let result;
+      if (!country) {
+        result = await apiRequest("https://covid19-api.org/api/timeline");
+      } else {
+        result = await apiRequest(
+          `https://covid19-api.org/api/timeline/${country}`
+        );
+      }
+      return result;
+    } catch (e) {
+      toast.error(
+        "API chart data error. Please reload the page or try later.",
+        TOAST_PARAMS
       );
+      throw e;
     }
-    return result;
   }
 );
 
 export const fetchCovidInfo = createAsyncThunk("covidInfo/set", async () => {
-  const apiResponse = await apiRequest("https://api.covid19api.com/summary");
-  const additionalInfo = await apiRequest(
-    "https://restcountries.eu/rest/v2/all?fields=alpha2Code;population;flag;latlng"
-  );
-  const countries = apiResponse.Countries;
-  const extendCountriesList = countries.map((item) => {
-    const countryId = additionalInfo.findIndex(
-      (el) => el.alpha2Code === item.CountryCode
+  try {
+    const apiResponse = await apiRequest("https://api.covid19api.com/summary");
+    const additionalInfo = await apiRequest(
+      "https://restcountries.eu/rest/v2/all?fields=alpha2Code;population;flag;latlng"
     );
-    const extendCountry = {
-      ...item,
-      flag: additionalInfo[countryId].flag,
-      population: additionalInfo[countryId].population,
-      geometry: additionalInfo[countryId].latlng,
+    const countries = apiResponse.Countries;
+    const extendCountriesList = countries.map((item) => {
+      const countryId = additionalInfo.findIndex(
+        (el) => el.alpha2Code === item.CountryCode
+      );
+      const extendCountry = {
+        ...item,
+        flag: additionalInfo[countryId].flag,
+        population: additionalInfo[countryId].population,
+        geometry: additionalInfo[countryId].latlng,
+      };
+      return extendCountry;
+    });
+    const worldPopulation = additionalInfo.reduce(
+      (acc, item) => acc + item.population,
+      0
+    );
+    return {
+      Global: { ...apiResponse.Global, worldPopulation },
+      Countries: extendCountriesList,
+      Date: apiResponse.Date,
     };
-    return extendCountry;
-  });
-  const worldPopulation = additionalInfo.reduce(
-    (acc, item) => acc + item.population,
-    0
-  );
-  return {
-    Global: { ...apiResponse.Global, worldPopulation },
-    Countries: extendCountriesList,
-    Date: apiResponse.Date,
-  };
+  } catch (e) {
+    toast.error(
+      "API Covid info error. Please reload the page or try later.",
+      TOAST_PARAMS
+    );
+    throw e;
+  }
 });
